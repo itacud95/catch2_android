c81e82c19e07c0a1099a4d02bf86ba5b1ade2b1f
jakob <jakobhkrohn@gmail.com>
2023-10-25 14:59:46 +0200

    Run tests from app

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,18 +1,28 @@
 #include <jni.h>
 #include <string>
 
+#include <android/log.h>
+
+// #include <catch2/catch_all.hpp>
+#define CATCH_CONFIG_RUNNER
+#include "catch2/catch_all.hpp"
+
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_unit_1tests_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "action bronson says hi";
-    return env->NewStringUTF(hello.c_str());
+    int result = Catch::Session().run( );
+    __android_log_print(ANDROID_LOG_INFO, "TRACKERS", "result: %d", result);
+    if (result != 0){
+        return env->NewStringUTF("test failure");
+    }
+    return env->NewStringUTF("test success");
 }
 
-#include <catch2/catch_all.hpp>
 
 TEST_CASE("Simple Test") {
-    REQUIRE(1 + 1 == 7);
+    __android_log_print(ANDROID_LOG_DEBUG, "TRACKERS", "%s", "Running test!");
+    REQUIRE(1 + 1 == 2);
 }
 
 
