3cfafe362c897d66716db5878a57c42e3ec72ca8
jakob <jakobhkrohn@gmail.com>
2023-11-09 12:42:43 +0100

    use test-runner

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -2,13 +2,31 @@
 #include <jni.h>
 
 #include <string>
+#include <dlfcn.h>
+
+#define log(...) __android_log_print(ANDROID_LOG_INFO, "TRACKERS", __VA_ARGS__);
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_unit_1tests_MainActivity_stringFromJNI(JNIEnv *env, jobject /* this */) {
-    // std::string failed_tests = "Failed tests:\n"; 
-    // __android_log_print(ANDROID_LOG_INFO, "TRACKERS", "result: %d", result);
-    // if (result != 0) {
-    //     return env->NewStringUTF(failed_tests.c_str());
-    // }
+    __android_log_print(ANDROID_LOG_INFO, "TRACKERS", "stringFromJNI: %d", 0);
+
+    void* handle = dlopen("libtest_runner.so", RTLD_NOW);
+    if (handle == nullptr) {
+        auto error_msg = "failed to open lib";
+        log("%s\n", error_msg);
+        return env->NewStringUTF(error_msg);
+    }
+
+    typedef bool (*RunTests)(int argc, char** argv);
+    RunTests runTests = nullptr;
+    runTests = reinterpret_cast<RunTests>(dlsym(handle, "RunTests"));
+    if (runTests == nullptr) {
+        auto error_msg = "failed to load runtests";
+        log("%s\n", error_msg);
+        return env->NewStringUTF(error_msg);
+    }
+
+    runTests(0, nullptr);
+
     return env->NewStringUTF("test success");
 }
